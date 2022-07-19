7568ba2fe9b8b91eacd36953ec2e2ffe385bcef3
Eric Ming <ericming@amazon.com>
2022-07-19 14:28:15 -0700

    Render cards from sample data

--- components/CardContainer.tsx
+++ components/CardContainer.tsx
@@ -1,13 +1,17 @@
 import React, { FunctionComponent } from 'react'
 import sampleData from '../assets/sampleData.json'
-import Card from '../components/Card.tsx'
+import Card from '../components/Card'
 
 const CardContainer: FunctionComponent = () => {
-    console.log(sampleData);
+    console.log(sampleData)
     //const data = JSON.stringify(sampleData)
-    return (
-        <Card />
-    )
+
+    const locationCards = sampleData.location.map((location) => (
+        <Card location={location} />
+    ))
+
+    return <div>{locationCards}</div>
+    //return <Card name="The Standard" details={sampleData['The Standard']} />
 }
 
 export default CardContainer
