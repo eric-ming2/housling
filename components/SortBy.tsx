08138a2d3482098c756d7d78775f6b9ad4047177
Eric Ming <ericming@amazon.com>
2022-07-25 14:47:07 -0700

    Add sort by options

--- components/SortBy.tsx
+++ components/SortBy.tsx
@@ -18,22 +18,68 @@ const SortBy: FunctionComponent<SortByProps> = (props) => {
         <>
             <div class="font-2xl font-medium pt-2">Sort By</div>
             <div class="flex place-items-center justify-between border border-solid border-gray-400 bg-white p-4 rounded shadow">
-                <svg
-                    class="h-8 w-8 text-gray-500"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke-width="2"
-                    stroke="currentColor"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
-                    {' '}
-                    <path stroke="none" d="M0 0h24v24H0z" />{' '}
-                    <path d="M18 15a6 6 0 1 1 -10.853 -3.529c1.926-2.338 4.763-3.327 3.848-8.47 2.355 1.761 5.84 5.38 2.022 9.406-1.136 1.091-.244 2.767 1.221 2.593.882-.105 2.023-.966 3.23-2.3.532.68.532 1.717.532 2.3z" />
-                </svg>
-                <div>Popular</div>
+                <div class="flex space-x-1 lg:space-x-4">
+                    <div class="text-gray-500 rounded-full p-2 font-black space-x-2 flex place-items-center hover:bg-gray-300">
+                        <svg
+                            class="h-8 w-8 text-gray-500"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                            />
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                            />
+                        </svg>
+
+                        <div>Popular</div>
+                    </div>
+                    <div class="text-gray-500 rounded-full p-2 font-black space-x-2 flex place-items-center hover:bg-gray-300">
+                        <svg
+                            class="h-8 w-8 text-gray-500"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            stroke-width="2"
+                            stroke="currentColor"
+                            fill="none"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            {' '}
+                            <path stroke="none" d="M0 0h24v24H0z" />{' '}
+                            <circle cx="12" cy="12" r="9" />{' '}
+                            <path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 0 0 0 4h2a2 2 0 0 1 0 4h-2a2 2 0 0 1 -1.8 -1" />{' '}
+                            <path d="M12 6v2m0 8v2" />
+                        </svg>
+                        <div>Price</div>
+                    </div>
+                    <div class="text-gray-500 rounded-full p-2 font-black space-x-2 flex place-items-center hover:bg-gray-300">
+                        <svg
+                            class="h-8 w-8 text-gray-500"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                            />
+                        </svg>
+
+                        <div>Rating</div>
+                    </div>
+                </div>
             </div>
         </>
     )
